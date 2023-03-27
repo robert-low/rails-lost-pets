9621182de15e7dfcd2ae8b3264285c7d1976e617
Robert Low <robert.low91@gmail.com>
2023-03-27 13:57:24 +0100

    delete

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -20,6 +20,24 @@ class PetsController < ApplicationController
     end
   end
 
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+    if @pet.update(pet_params)
+      redirect_to pet_path(@pet)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pet = Pet.find(params[:id])
+    @pet.destroy
+    redirect_to pets_path, status: :see_other
+  end
 
   private
 
